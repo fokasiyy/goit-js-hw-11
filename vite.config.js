2186b9686adf5c57fb605143ca53632c9dd3e050
fokasiyy <foka123zate@gmail.con>
2025-06-05 13:11:14 +0300

    Внёс правки и обновления

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/goit-js-hw-11/',
     build: {
       sourcemap: true,
       rollupOptions: {
@@ -46,3 +47,4 @@ export default defineConfig(({ command }) => {
     ],
   };
 });
+
